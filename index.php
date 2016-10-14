3cbf9a5fc2e7fe474b37a881599ebc6a5c5b4ee2
xgxofdream <liujie.dhu@gmail.com>
2016-10-14 20:54:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,13 @@ session_start ();
 include 'db/db_config.php';
 include 'config/global.php';
 include 'functions/function.php';
-//应用的根目录就是index.php的父目录
+
 define("SERVER_ROOT", dirname(__FILE__));
 
-//你的域名.comm 是你的服务器域名
 define('SITE_ROOT' , 'http://localhost/');
 
 /**
- * 引入router.php
+ * Introduce router.php to assign needs
  */
 require_once(SERVER_ROOT . '/controllers/' . 'router.php');
 
