c3bb90e882747f1e7151bb18162c5cc02b6966ee
xgxofdream <liujie.dhu@gmail.com>
2016-10-14 20:55:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php 
 session_start ();
-
+/**
+ * Introduce basic files
+ */
 include 'db/db_config.php';
 include 'config/global.php';
 include 'functions/function.php';
-
+/**
+ * Define ROOT level variables
+ */
 define("SERVER_ROOT", dirname(__FILE__));
-
 define('SITE_ROOT' , 'http://localhost/');
 
 /**
